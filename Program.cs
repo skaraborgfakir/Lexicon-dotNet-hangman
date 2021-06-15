085a0ebf374163e9f39ab704aed7f1847769b7b4
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-06-15 13:53:39 +0200

    hängagubbe-spel

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace hangmanspel
+{
+    class Program
+    {
+	Array<string> ordlist = new Array<string> ( "xylofon",
+						    "säckpipa",
+						    "harpa",
+						    "vibrafon",
+						    "puka",
+						    "tenorsaxofon",
+						    "saxofon",
+						    "barytonsaxofon",
+						    "flöjt",
+						    "tvärflöjt",
+						    "kontrabas",
+						    "hardangerfela",
+						    "träskofiol",
+						    "nyckelharpa",
+						    "viola",
+						    "cello",
+						    "basfiol");
+
+	static void Main(string[] args)
+	{
+	    Console.WriteLine("Hello World!");
+	}
+    }
+}
